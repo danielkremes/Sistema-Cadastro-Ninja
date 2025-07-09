3fc8dac8ed7339818527f788fdb5302d7722f710
danielkremes <danielkremes06@gmail.com>
2025-07-09 17:11:26 -0300

    feat: add annotation para mapping class Controller

--- src/main/java/br/com/registration/sistema_cadastro_ninjas/controller/Controller.java
+++ src/main/java/br/com/registration/sistema_cadastro_ninjas/controller/Controller.java
@@ -1,7 +1,10 @@
 package br.com.registration.sistema_cadastro_ninjas.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("v1/ninjas/")
 public class Controller {
+
 }
