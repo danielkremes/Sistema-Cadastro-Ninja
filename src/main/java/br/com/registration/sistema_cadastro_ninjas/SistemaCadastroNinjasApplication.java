f0378726c14d7319e3a67a63302db757125a1d00
danielkremes <danielkremes06@gmail.com>
2025-07-09 16:56:51 -0300

    chore: initial project version

---
+++ src/main/java/br/com/registration/sistema_cadastro_ninjas/SistemaCadastroNinjasApplication.java
@@ -0,0 +1,13 @@
+package br.com.registration.sistema_cadastro_ninjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaCadastroNinjasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaCadastroNinjasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/registration/sistema_cadastro_ninjas/SistemaCadastroNinjasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.registration.sistema_cadastro_ninjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaCadastroNinjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
